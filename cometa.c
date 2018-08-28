c0ae65d456f8be40f0edc0aeeb5deb55a6e5b171
Edely <edely.gomes@convergenceworks.com.br>
2018-08-28 11:32:10 -0300

    accepted

--- cometa.c
+++ cometa.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
 	int ano_atual, proximo_ano, remainder;
 
-	scanf("%d", &ano_atual);
+	scanf("%i", &ano_atual);
 	remainder = ano_atual % 76;
 	if ((ano_atual >= 2010) && (ano_atual <= 10 * 10 * 10 * 10)) {
 
 		if (remainder == 10) {
 			proximo_ano = ano_atual + 76;
 		}
+		else if(remainder < 10) {
+			proximo_ano = ano_atual + (10 - remainder);
+		}
 		else {
 			proximo_ano = ano_atual + 86 - remainder;
 		}
-		printf("%d", proximo_ano);
+		printf("%i", proximo_ano);
 	}
 
 	return 0;
