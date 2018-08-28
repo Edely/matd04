8ba4adcd492dd4e865c871e6fef3b96ff686f2d3
Edely <edely.gomes@convergenceworks.com.br>
2018-08-28 11:06:12 -0300

    codigo funciona, nao passa no moj

--- cometa.c
+++ cometa.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
 	int ano_atual, proximo_ano, remainder;
 
-	//ano_atual = atoi(argv[1]);
-	scanf('%d', ano_atual);
+	scanf("%d", &ano_atual);
 	remainder = ano_atual % 76;
 	if ((ano_atual >= 2010) && (ano_atual <= 10 * 10 * 10 * 10)) {
 
@@ -16,9 +14,8 @@ int main(int argc, char *argv[]) {
 		else {
 			proximo_ano = ano_atual + 86 - remainder;
 		}
+		printf("%d", proximo_ano);
 	}
-
-	printf("%i", proximo_ano);
 
 	return 0;
 }
