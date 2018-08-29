8439c29a5f620bf615505f7dd0340582ca5bbae3
Edely <edely.gomes@convergenceworks.com.br>
2018-08-29 13:03:21 -0300

    recebendo dados e testando corretamente - precisa acessar lista dinamica

--- vo.c
+++ vo.c
@@ -2,32 +2,40 @@
 
 int main(){
 	
-	int linhas, colunas, i, j, temp, *int_entrada, *int_resultados;	
+	int linhas, colunas, i, j, num_teste, *int_entrada, *int_resultados, temp, a, b;	
 
 	scanf("%d", &linhas);
-
+	num_teste = 1;
 	colunas = 1;
+	temp = 0;
 
-	int_entrada = (int *)malloc(sizeof(int) * colunas * linhas);	
-	int_resultados = (int *)malloc(linhas * sizeof(int));
-	
-	for(i = 0; i < linhas; i++) {
-		for(j = 0; j < colunas; j+=2) {
-			printf("Type the matrix numbers\n");
-			scanf("%d %d", &int_entrada[i*linhas +j], &int_entrada[i*linhas + j+1]);			
+	while (linhas != 0) {
+		int_entrada = (int *)malloc(sizeof(int) * colunas * linhas);
+		int_resultados = (int *)malloc(linhas * sizeof(int));
+
+		for (i = 0; i < linhas; i++) {
+			for (j = 0; j < colunas; j += 2) {
+				scanf("%d %d", &int_entrada[i*linhas + j], &int_entrada[i*linhas + j + 1]);
+			}
 		}
+
+		printf("Teste %d \n", num_teste);
+		for (i = 0; i < linhas; i++) {
+			for (j = 0; j < colunas; j++) {
+				temp = temp + int_entrada[i*linhas + j] - int_entrada[i*linhas + j + 1];
+				printf("%d\n", temp);
+			}
+		}
+		printf("\n");
+
+		free(int_entrada);
+		free(int_resultados);
+
+		num_teste++;
+		temp = 0;
+		scanf("%d", &linhas);
 	}
 
-
-	printf("Teste\n");
-	for (i = 0; i < linhas; i++) {
-		for (j = 0; j < colunas; j++) {
-			printf("Number: %d %d \n", int_entrada[i*linhas + j], int_entrada[i*linhas + j + 1]);
-		}
-	}
-
-	free(int_entrada);
-	free(int_resultados);
 	return 0;
 
 }
