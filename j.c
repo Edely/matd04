34341bcfd1153669a2c88c4444b5d4ecdd879a5d
Edely <edely.gomes@convergenceworks.com.br>
2018-09-04 09:33:14 -0300

    lendo e imprimindo nomes corretamente

--- j.c
+++ j.c
@@ -1,17 +1,47 @@
-//S pedaços de pergaminhos numerados de 1 a S
-//sorteia numero I
-// ordena por ordem alfabética
-//seleciona o I-ésimo por ordem alfabética
+ï»¿// S pedaÃ§os de pergaminhos numerados de 1 a S
+// sorteia numero I
+
+//1 â‰¤ I â‰¤ S â‰¤ 100
+
+// ler primeira linha
+// ler demais nomes
+// ordena por ordem alfabetica
+// seleciona o I-esimo por ordem alfabetica
 
 #include <stdio.h>
+#include <stdlib.h>
 
-//ler primeira linha
+struct soldado{
+	char nome[20];
+};
+
+typedef struct soldado t_soldado;
 
 int main(int argc, char *argv[]) {
-
-	int S I;
+	//ler primeira linha
+	int S, I, i;
+	char temp;
 	scanf("%d %d", &S, &I);
-	printf("%d %d", S, I);
+	t_soldado lista_soldados[100];
+
+	printf("\n");
+	//printf("%d %d\n", S, I);
+	printf("Valor de I: %dn", I);
+
+	//ler os nomes
+	for(i = 0; i < S; i++){
+		printf("%d\n", i);
+		scanf("%c", &temp);
+		scanf("%[^\n]", (lista_soldados[i]).nome );
+	}
+
+	printf("\n");
+
+	for (i = 0; i < S; i++) {
+		if ((i + 1) == I)
+			printf("\nGANHADOOOOORRRR, POORRAAAA!!!\n");
+		printf("%s\n", (lista_soldados[i]).nome);
+	}
 	
 	return 0;
 }
--- teste.c
+++ teste.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 struct {
 	int teste;
 } x;
@@ -9,5 +9,17 @@ int main() {
 
 	printf("%i", x.teste);
 
+	return 0;
+}
+
+*/
+
+int main()
+{
+	char name[30];
+	printf("Enter name: ");
+	scanf("%[^\n]", name);
+
+	printf("Name is: %s\n", name);
 	return 0;
 }
