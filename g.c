b916963566ea1676c123f762d1b011e07b85a0fc
edely <edely.gomes@gmail.com>
2018-09-08 08:50:21 -0300

    decidindo casos corretamente

--- g.c
+++ g.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
 
@@ -10,10 +11,21 @@ int main(int argc, char *argv[]){
     while(! ((N == 0) && (P == 0)) ){
 
         printf("%d %d\n", N, P);
+
         inteiro = N / P;
         inteiro += N % P > 0 ? 1: 0;
+
         printf("Inteiro: %d\n", inteiro);
 
+        if(inteiro <=6){
+            printf("Poodle\n");
+        }
+        else if(inteiro>=20){
+            printf("Poooooooooooooooodle\n");
+        }
+        else{
+            printf("tem que ver isso ai\n");
+        }
         scanf("%d %d", &N, &P);
     }
     return 0;
