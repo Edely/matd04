cdd54b4c42b8ab5ea62b24866cb7774ebc93bf35
edely <edely.gomes@gmail.com>
2018-09-08 10:20:33 -0300

    vovo -e accepted

--- e.c
+++ e.c
@@ -8,17 +8,14 @@ int main(int argc, char *argv[]){
     scanf("%d", &N);
     j = 0;
     while(N != 0){
-        printf("aqui");
         resto = 0;
         j++;        
         printf("Teste %i\n", j);     
         for(i = 0; i < N; i++){
-            printf("i %d\n", i);    
             scanf("%d %d", &joao, &pedro);
             resto += joao - pedro;
             printf("%d\n", resto);        
         }
-        printf("digite n\n");
         scanf("%d", &N);
     }
 
