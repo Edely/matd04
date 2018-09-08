8be04482633c00648652cafe63a232e9ef4e27f4
edely <edely.gomes@gmail.com>
2018-09-08 10:09:13 -0300

    consertando scanf

--- e.c
+++ e.c
@@ -3,22 +3,23 @@
 
 int main(int argc, char *argv[]){
 
-    int N, joao, pedro, resto, i;
+    int N, joao, pedro, resto, i, j;
     
-    scanf("%d\n", &N);
-
-    
+    scanf("%d", &N);
+    j = 0;
     while(N != 0){
+        printf("aqui");
         resto = 0;
+        j++;        
+        printf("Teste %i\n", j);     
         for(i = 0; i < N; i++){
-            scanf("%d %d\n", &joao, &pedro);
+            printf("i %d\n", i);    
+            scanf("%d %d", &joao, &pedro);
             resto += joao - pedro;
-        
-            printf("resto\n");     
             printf("%d\n", resto);        
         }
-        
-        scanf("%d\n", &N);
+        printf("digite n\n");
+        scanf("%d", &N);
     }
 
     return 0;
